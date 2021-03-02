d6160ad19b505c3f92cd62daaa0b24afd3e34c57
aksharasada <73641210+aksharasada@users.noreply.github.com>
2021-03-02 18:06:20 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,63 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	ball1 = new Ball(200, 300, 50);
+	ball2 = new Ball(300, 300, 50);
+	ball3 = new Ball(400, 300, 50);
+	ball4 = new Ball(500, 300, 50);
+	ball5 = new Ball(600, 300, 50);
+
+	rope1 = new Rope(ball1.body, {x: 200, y: 100});
+	rope2 = new Rope(ball2.body, {x: 300, y: 100});
+	rope3 = new Rope(ball3.body, {x: 400, y: 100});
+	rope4 = new Rope(ball4.body, {x: 500, y: 100});
+	rope5 = new Rope(ball5.body, {x: 600, y: 100});
+
+	roof = new Roof(400, 100, 500, 10);
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  Engine.update(engine)
+  
+ ball1.display();
+ ball2.display();
+ ball3.display();
+ ball4.display();
+ ball5.display();
+
+ rope1.display();
+ rope2.display();
+ rope3.display();
+ rope4.display();
+ rope5.display();
+
+ roof.display();
+}
+
+function mouseDragged(){
+	Matter.Body.setPosition(ball1.body, {x: mouseX, y: mouseY})
+	}
+	
+
